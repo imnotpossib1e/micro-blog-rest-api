79aaa7b4a07b9760b65731dc1a58879762e7f01d
imnotpossib1e <habit2me@gmail.com>
2026-06-23 22:53:47 +0900

    ♻️ refactor: 에러 메시지 수정

--- src/main/java/com/asdf/minilog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/asdf/minilog/exception/GlobalExceptionHandler.java
@@ -24,7 +24,7 @@ public class GlobalExceptionHandler {
             })
     @ExceptionHandler(UserNotFoundException.class) // UserNotFoundException이 발생하면 자동으로 여기로 와서 처리한다
     public ResponseEntity<String> handleUserNotFoundException (UserNotFoundException ex) { // HTTP 응답 커스터마이징
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND); // 실제 응답 부분
+        return new ResponseEntity<>("해당 사용자를 찾을 수 없습니다.", HttpStatus.NOT_FOUND); // 실제 응답 부분
     }
 
     // 개사글 예외 처리
@@ -42,7 +42,7 @@ public class GlobalExceptionHandler {
             })
     @ExceptionHandler(ArticleNotFoundException.class)
     public ResponseEntity<String> handleArticleNotFoundException (ArticleNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("해당 게시글을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
     }
 
     // 잘못된 값이 들어왔을 때 자바에서 기본으로 던지는 에러 처리
@@ -57,7 +57,7 @@ public class GlobalExceptionHandler {
             })
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException (IllegalArgumentException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("잘못된 요청 값입니다.", HttpStatus.BAD_REQUEST);
     }
 
     // 지금까지 따로 처리 안 된 모든 예외 처리
@@ -69,8 +69,8 @@ public class GlobalExceptionHandler {
             })
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException (Exception ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-//        return new ResponseEntity<>(  "서버 오류가 발생했습니다", HttpStatus.INTERNAL_SERVER_ERROR);
+//        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         // 내부 에러 메시지를 그대로 노출하기보다는 따로 적어주는게 좋다
+        return new ResponseEntity<>(  "서버 오류가 발생했습니다", HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
