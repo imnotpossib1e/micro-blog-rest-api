60d8dce0606184774fa7ef78187918f0e8a7d7d1
imnotpossib1e <habit2me@gmail.com>
2026-06-24 15:05:46 +0900

    :sparkles: feat: Follow 엔티티 DTO 변환 메서드 추가

--- src/main/java/com/asdf/minilog/dto/FollowResponseDto.java
+++ src/main/java/com/asdf/minilog/dto/FollowResponseDto.java
@@ -1,12 +1,14 @@
 package com.asdf.minilog.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class FollowResponseDto {
-    @NonNull private Long followerId;
-    @NonNull private Long followeeId;
+    @NonNull private Long followerId; // 팔로워 ID
+    @NonNull private Long followeeId; // 팔로이 ID
 }
--- src/main/java/com/asdf/minilog/util/EntityDtoMapper.java
+++ src/main/java/com/asdf/minilog/util/EntityDtoMapper.java
@@ -6,6 +6,7 @@ import com.asdf.minilog.entity.Article;
 import com.asdf.minilog.entity.Follow;
 
 public class EntityDtoMapper {
+    // Artcle Entity -> ArticleResponseDto 매퍼
     public static ArticleResponseDto toDto(Article article){
         return ArticleResponseDto.builder()
                 .articleId(article.getId()) // 게시글 ID
@@ -13,6 +14,14 @@ public class EntityDtoMapper {
                 .authorId(article.getAuthor().getId()) // 작성자 ID
                 .authorName(article.getAuthor().getUsername()) // 작성자 이름
                 .createdAt(article.getCreatedAt()) // 게시글 생성 시
+                .build();
+    }
+
+    // Follow Entity -> FollowResponseDTo 매퍼
+    public static FollowResponseDto toDto(Follow follow) {
+        return FollowResponseDto.builder()
+                .followerId(follow.getFollower().getId()) // 팔로워 ID
+                .followeeId(follow.getFollowee().getId()) // 팔로이 ID
                 .build();
     }
 }
