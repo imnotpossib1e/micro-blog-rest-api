fb31b863b51e813aeb7aa4450dbac78247e3c8f7
imnotpossib1e <habit2me@gmail.com>
2026-06-24 16:32:42 +0900

    :sparkles: feat: FollowRepository 작성

---
+++ src/main/java/com/asdf/minilog/repository/FollowRepository.java
@@ -0,0 +1,18 @@
+package com.asdf.minilog.repository;
+
+import com.asdf.minilog.entity.Follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+// 누가 누구를 팔로우하는지 조회/검사
+@Repository
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    // 특정 사용자가 팔로우한 목록 조회
+    List<Follow> findByFollowerId(Long followerId);
+
+    // A가 B를 팔로우하고 있는지 확인
+    Optional<Follow> findByFollowerIdAndFolloweeId(Long followerId, Long followeeId);
+}
