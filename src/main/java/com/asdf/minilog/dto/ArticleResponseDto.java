2a28ab18f464cde71de0768ec88483d7bbb8c5fe
imnotpossib1e <habit2me@gmail.com>
2026-06-24 14:40:22 +0900

    :sparkles: feat: 게시글 작성, 수정, 조회에 대한 응답 DTO

--- src/main/java/com/asdf/minilog/dto/ArticleResponseDto.java
+++ src/main/java/com/asdf/minilog/dto/ArticleResponseDto.java
@@ -1,4 +1,19 @@
 package com.asdf.minilog.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
 public class ArticleResponseDto {
+    @NonNull private Long articleId;
+    @NonNull private String content;
+    @NonNull private Long authorId;
+    @NonNull private String authorName;
+    @NonNull private LocalDateTime createdAt;
 }
