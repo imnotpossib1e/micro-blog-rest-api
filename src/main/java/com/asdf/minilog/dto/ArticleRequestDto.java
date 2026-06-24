7e7ab1095b5ddf4cf42f9a1b59f300762cc11dfd
imnotpossib1e <habit2me@gmail.com>
2026-06-24 14:32:56 +0900

    :sparkles: feat: 게시글 작성, 수정 요청을 위한 DTO 작성

--- src/main/java/com/asdf/minilog/dto/ArticleRequestDto.java
+++ src/main/java/com/asdf/minilog/dto/ArticleRequestDto.java
@@ -1,4 +1,15 @@
 package com.asdf.minilog.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
 public class ArticleRequestDto {
+    @NotBlank private String content;
+    @NotNull private Long authorId;
 }
